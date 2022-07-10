d6d52473745deaa902bee2dcd1eeb66578e4348a
oladipoacademics <50375486+oladipoacademics@users.noreply.github.com>
2022-07-10 10:14:49 +0100

    Add files via upload
    Arduino file

---
+++ Oladipo_bmp180.ino
@@ -0,0 +1,327 @@
+/*
+ * 
+ * Edited by Oladipo Mumin O.
+ * 
+ * to read  bmp 180, dht11, and display the values on LCD then upload the code to the cloud 
+ * 
+ * Precip water vapour and mean weight temperature model was provided by Sukam
+ * 
+  
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/visualize-esp32-esp8266-sensor-readings-from-anywhere/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+*/
+
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h> 
+
+
+ LiquidCrystal_I2C lcd(0x27,16,2);   
+ // if lcd is not print then use this 0x3F..  
+
+char status;
+  double T,Tm,P,p0,a, pv, H;
+// You will need to create an SFE_BMP180 object, here called "pressure":
+
+SFE_BMP180 pressure;
+
+#define ALTITUDE 230.0 // Altitude of Ibadan in meters
+//#include <Arduino.h>
+//#include <Hash.h>
+//#include <ESPAsyncTCP.h>
+//#include <ESPAsyncWebServer.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE    DHT11     // DHT 11 
+DHT dht(DHTPIN, DHTTYPE);
+float t = 0.0;
+float h = 0.0;  
+
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#endif
+
+#include <Wire.h>
+//#include <Adafruit_Sensor.h>
+//#include <Adafruit_BME280.h>
+
+// Replace with your network credentials
+const char* ssid     = "db1 Pro";
+const char* password = "123456789";
+
+//const char* ssid     = "OlaSolar";
+//const char* password = "OLAdipo1";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http://naconf.000webhostapp.com/post-data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+/*#include <SPI.h>
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
+
+//Adafruit_BME280 bme;  // I2C
+//Adafruit_BME280 bme(BME_CS);  // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);  // software SPI
+
+void setup() {
+  
+  lcd.init();      
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(1,0);
+ lcd.print("KDU Weather");
+ 
+  lcd.setCursor(4,1);
+  lcd.print("Station");
+    
+  Serial.begin(115200);
+  dht.begin();
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize the sensor (it is important to get calibration values stored on the device).
+// lcd.begin();      
+ //lcd.backlight();  
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+    
+    
+  }
+}
+
+void loop() {
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(client, serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    //take readings from sensor
+
+    read_sensor();
+    read_sensor_2();
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(T)
+                           + "&value2=" + String(Tm) + "&value3=" + String(H) + "&value4=" + String(pv) +  "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = http.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //http.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+    
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 15 seconds
+  delay(15000);  
+}
+
+//SUBROUTINE TO READ SENSOR
+void read_sensor(){
+
+   
+
+  // Loop here getting pressure readings every 10 seconds.
+
+  // If you want sea-level-compensated pressure, as used in weather reports,
+  // you will need to know the altitude at which your measurements are taken.
+  // We're using a constant called ALTITUDE in this sketch:
+  
+  Serial.println();
+  Serial.print("provided altitude: ");
+  Serial.print(ALTITUDE,0);
+  Serial.print(" meters, ");
+  Serial.print(ALTITUDE*3.28084,0);
+  Serial.println(" feet");
+  
+  // If you want to measure altitude, and not pressure, you will instead need
+  // to provide a known baseline pressure. This is shown at the end of the sketch.
+
+  // You must first get a temperature measurement to perform a pressure reading.
+  
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+      T = T + 273;
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+      Tm = ((0.4373*T) + 161.46);
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+
+          // The pressure sensor returns abolute pressure, which varies with altitude.
+          // To remove the effects of altitude, use the sealevel function and your current altitude.
+          // This number is commonly used in weather reports.
+          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+          // Result: p0 = sea-level compensated pressure in mb
+
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+
+          // On the other hand, if you want to determine your altitude from the pressure reading,
+          // use the altitude function along with a baseline pressure (sea-level or other).
+          // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+          // Result: a = altitude in m.
+
+          a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+  delay(2000);  // Pause for 2 seconds.
+  
+  }
+
+
+  void read_sensor_2(){
+    
+  //  DHT.read11(dht_apin);
+  float h = dht.readHumidity();
+//  float t = dht.readTemperature();
+ 
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temp = ");
+  lcd.print(T);
+  lcd.print(" K");
+  lcd.setCursor(0,1);
+  lcd.print("Tm = ");
+  lcd.print(Tm);
+  lcd.print(" K");
+  delay(13000); 
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+ lcd.print("RH = ");
+ H=h;
+  lcd.print(H);
+  lcd.print(" %");
+  lcd.setCursor(0,1);
+  pv = ((-0.007276*(h*h))+ (1.365*(h))- 16.4);
+  lcd.print("PWV = ");
+  lcd.print(pv);
+  lcd.print(" mm");
+  Serial.println(h);
+  Serial.print("humdity");
+  delay(1000);
+    }
